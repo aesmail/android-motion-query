427a679ff921ae15bf5c31a4f755f02dbb47a120
Abdullah Esmail <abdullah.esmail@gmail.com>
2017-11-03 05:54:37 +0300

    fix dir path.

--- lib/android_query.rb
+++ lib/android_query.rb
@@ -4,7 +4,7 @@ unless defined?(Motion::Project::Config)
   raise "This gem works only within a RubyMotion android project."
 end
 
-lib_dir_path = File.dirname(File.expands_path(__FILE__))
+lib_dir_path = File.dirname(File.expand_path(__FILE__))
 
 Motion::Project::App.setup do |app|
   app.files.unshift(Dir.glob(File.join(lib_dir_path, "android_query/**/*.rb")))
