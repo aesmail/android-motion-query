c94e7ab0bf5ced2515074b95187e3c5f63080067
Abdullah Esmail <abdullah.esmail@gmail.com>
2017-11-06 02:14:42 +0300

    changed file name because RubyMotion has problems with class name = filename.

--- lib/android_query/views.rb
+++ lib/android_query/views.rb
@@ -2,11 +2,12 @@ module AndroidMotionQuery
   class View
     attr_accessor :view, :activity, :stylesheet, :style_name, :layout_params, :options
 
-    def initialize(view, activity, stylesheet, style_name, options = {})
+    def initialize(view, activity, stylesheet, style_name, layout_params, options = {})
       self.view = view
       self.activity = activity
       self.stylesheet = stylesheet
       self.style_name = style_name
+      self.layout_params = layout_params
       self.options = {
         parent: nil,
       }.merge(options)
@@ -16,37 +17,41 @@ module AndroidMotionQuery
       self.view
     end
 
-    def create_android_query_view(view, style_method, options = {}, &block)
-      aqv = View.new(view, self.activity, self.stylesheet, style_method, options)
-      self.stylesheet.apply_style_for(aqv, style_method)
+    def create_android_query_view(view, style_method, layout_params, options = {}, &block)
+      aqv = View.new(view, self.activity, self.stylesheet, style_method, layout_params, options)
+      self.stylesheet.apply_style_for(aqv, style_method, layout_params)
       self.get.addView(aqv.get)
       block.call(aqv) if block_given?
       aqv
     end
+    
+    def linear_layout(style_method, &block)
+      view = Android::Widget::LinearLayout.new(self.activity)
+      lp = Android::Widget::LinearLayout::LayoutParams
+      create_android_query_view(view, style_method, lp, {}, &block)
+    end
 
     def text_view(style_method, &block)
       view = Android::Widget::TextView.new(self.activity)
-      create_android_query_view(view, style_method, {}, &block)
+      create_android_query_view(view, style_method, self.layout_params, {}, &block)
     end
     
     def edit_text(style_method, &block)
       view = Android::Widget::EditText.new(self.activity)
-      create_android_query_view(view, style_method, {}, &block)
+      create_android_query_view(view, style_method, self.layout_params, {}, &block)
     end
     
     def button(style_method, &block)
       view = Android::Widget::Button.new(self.activity)
-      create_android_query_view(view, style_method, {}, &block)
+      create_android_query_view(view, style_method, self.layout_params, {}, &block)
     end
   end
 
   class Stylesheet
-    def apply_style_for(view, style_name)
-      style_view = StylesheetElement.new(view)
+    def apply_style_for(view, style_name, layout_params)
+      style_view = StylesheetElement.new(view, layout_params)
       self.send(style_name.to_s, style_view)
-      puts 'applying layout params to view...'
       view.get.setLayoutParams(style_view.params)
-      puts 'applying params OK...'
       view
     end
   end
@@ -64,19 +69,17 @@ module AndroidMotionQuery
       horizontal: Android::Widget::LinearLayout::HORIZONTAL,
     }
 
-    def initialize(view)
+    def initialize(view, layout_params)
       self.view = view
-      self.params = Android::View::ViewGroup::LayoutParams.new(LAYOUT_SIZE_OPTIONS[:mp], LAYOUT_SIZE_OPTIONS[:wc])
+      self.params = layout_params.new(LAYOUT_SIZE_OPTIONS[:mp], LAYOUT_SIZE_OPTIONS[:wc])
       self
     end
 
     def text=(t)
-      puts 'setting text for view...'
       self.view.get.text = t
     end
 
     def width=(w)
-      puts 'Setting width...'
       if w == :mp || w == :wc
         self.params.width = LAYOUT_SIZE_OPTIONS[w]
       else
@@ -85,7 +88,6 @@ module AndroidMotionQuery
     end
 
     def height=(h)
-      puts 'Setting height...'
       if h == :mp || h == :wc
         self.params.height = LAYOUT_SIZE_OPTIONS[h]
       else
@@ -94,7 +96,6 @@ module AndroidMotionQuery
     end
 
     def orientation=(o)
-      puts 'Setting orientation...'
       if o == :vertical || o == :horizontal
         self.view.get.orientation = ORIENTATION_OPTIONS[o]
       end
@@ -136,14 +137,15 @@ class AndroidQuery
     self
   end
   
-  def create_android_query_view(view, style_method, options = {})
-    AndroidMotionQuery::View.new(view, self.activity, self.stylesheet, style_method, options)
+  def create_android_query_view(view, style_method, layout_params, options = {})
+    AndroidMotionQuery::View.new(view, self.activity, self.stylesheet, style_method, layout_params, options)
   end
   
   def linear_layout(style_method, &block)
     view = Android::Widget::LinearLayout.new(self.activity)
-    aqv = create_android_query_view(view, style_method, {})
-    self.stylesheet.apply_style_for(aqv, style_method)
+    layout_params = Android::Widget::LinearLayout::LayoutParams
+    aqv = create_android_query_view(view, style_method, layout_params, {})
+    self.stylesheet.apply_style_for(aqv, style_method, layout_params)
     block.call(aqv) if block_given?
     self.activity.setContentView(aqv.get)
   end
@@ -160,6 +162,12 @@ class AndroidQuery
       right: Android::View::Gravity::RIGHT,
       bottom: Android::View::Gravity::BOTTOM,
       center: Android::View::Gravity::CENTER,
+      bottom_right: Android::View::Gravity::BOTTOM | Android::View::Gravity::RIGHT,
+      bottom_left: Android::View::Gravity::BOTTOM | Android::View::Gravity::LEFT,
+      center_right: Android::View::Gravity::CENTER | Android::View::Gravity::RIGHT,
+      center_left: Android::View::Gravity::CENTER | Android::View::Gravity::LEFT,
+      top_right: Android::View::Gravity::TOP | Android::View::Gravity::RIGHT,
+      top_left: Android::View::Gravity::TOP | Android::View::Gravity::LEFT,
     }
 
     length_options = {
